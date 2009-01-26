eebf0f075e7a10fccb1ea934f02de1916e20a074
Steven Whitehouse <swhiteho@redhat.com>
2009-01-26 10:13:56 +0000

    libgfs2: Merge linux_endian.h into libgfs2
    Its not very large, and we always need it anyway.
    
    Signed-off-by: Steven Whitehouse <swhiteho@redhat.com>

--- gfs2/libgfs2/libgfs2.h
+++ gfs2/libgfs2/libgfs2.h
@@ -12,8 +12,9 @@
 #include <sys/types.h>
 #include <linux/types.h>
 #include <linux/limits.h>
+#include <endian.h>
+#include <byteswap.h>
 
-#include "linux_endian.h"
 #include <linux/gfs2_ondisk.h>
 #include "osi_list.h"
 #include "copyright.cf"
@@ -28,6 +29,64 @@ __BEGIN_DECLS
 #ifndef FALSE
 #define FALSE (0)
 #endif
+
+/*  I'm not sure which versions of alpha glibc/gcc are broken,
+    so fix all of them.  */
+#ifdef __alpha__
+#undef bswap_64
+static __inline__ unsigned long bswap_64(unsigned long x)
+{
+  unsigned int h = x >> 32;
+  unsigned int l = x;
+
+  h = bswap_32(h);
+  l = bswap_32(l);
+
+  return ((unsigned long)l << 32) | h;
+}
+#endif  /*  __alpha__  */
+
+
+#if __BYTE_ORDER == __BIG_ENDIAN
+
+#define be16_to_cpu(x) (x)
+#define be32_to_cpu(x) (x)
+#define be64_to_cpu(x) (x)
+
+#define cpu_to_be16(x) (x)
+#define cpu_to_be32(x) (x)
+#define cpu_to_be64(x) (x)
+
+#define le16_to_cpu(x) (bswap_16((x)))
+#define le32_to_cpu(x) (bswap_32((x)))
+#define le64_to_cpu(x) (bswap_64((x)))
+
+#define cpu_to_le16(x) (bswap_16((x)))
+#define cpu_to_le32(x) (bswap_32((x)))
+#define cpu_to_le64(x) (bswap_64((x)))
+
+#endif  /*  __BYTE_ORDER == __BIG_ENDIAN  */
+
+
+#if __BYTE_ORDER == __LITTLE_ENDIAN
+
+#define be16_to_cpu(x) (bswap_16((x)))
+#define be32_to_cpu(x) (bswap_32((x)))
+#define be64_to_cpu(x) (bswap_64((x)))
+
+#define cpu_to_be16(x) (bswap_16((x)))
+#define cpu_to_be32(x) (bswap_32((x)))
+#define cpu_to_be64(x) (bswap_64((x))) 
+
+#define le16_to_cpu(x) (x)
+#define le32_to_cpu(x) (x)
+#define le64_to_cpu(x) (x)
+
+#define cpu_to_le16(x) (x)
+#define cpu_to_le32(x) (x)
+#define cpu_to_le64(x) (x)
+
+#endif  /*  __BYTE_ORDER == __LITTLE_ENDIAN  */
 
 extern char *prog_name;
 
