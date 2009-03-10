ec9ec0451669c081bde50ad41280b94aa2e0c11b
David Teigland <teigland@redhat.com>
2009-03-10 15:53:11 -0500

    gfs_controld: dlm_controld failure is cluster_dead
    Losing the dlm_controld connection is equivalent to a cluster failure.
    
    Signed-off-by: David Teigland <teigland@redhat.com>

--- group/gfs_controld/main.c
+++ group/gfs_controld/main.c
@@ -1041,13 +1041,6 @@ void cluster_dead(int ci)
 	cluster_down = 1;
 }
 
-static void dlmcontrol_dead(int ci)
-{
-	if (!list_empty(&mountgroups))
-		log_error("dlm_controld is gone");
-	client_dead(ci);
-}
-
 static void loop(void)
 {
 	int poll_timeout = -1;
@@ -1096,7 +1089,7 @@ static void loop(void)
 	rv = setup_dlmcontrol();
 	if (rv < 0)
 		goto out;
-	client_add(rv, process_dlmcontrol, dlmcontrol_dead);
+	client_add(rv, process_dlmcontrol, cluster_dead);
 
 	for (;;) {
 		rv = poll(pollfd, client_maxi + 1, poll_timeout);
