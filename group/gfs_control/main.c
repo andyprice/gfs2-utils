4e7c66348809fa494563f622a24f6049dcf23dd0
Fabio M. Di Nitto <fdinitto@redhat.com>
2009-05-06 09:08:50 +0200

    gfs_control: make functions static
    Signed-off-by: Fabio M. Di Nitto <fdinitto@redhat.com>

--- group/gfs_control/main.c
+++ group/gfs_control/main.c
@@ -156,7 +156,7 @@ static int do_write(int fd, void *buf, size_t count)
 	return 0;
 }
 
-void do_leave(char *table)
+static void do_leave(char *table)
 {
 	struct gfsc_mount_args ma;
 	int rv;
@@ -170,7 +170,7 @@ void do_leave(char *table)
 		fprintf(stderr, "gfs_controld leave error %d\n", rv);
 }
 
-char *mg_flags_str(uint32_t flags)
+static char *mg_flags_str(uint32_t flags)
 {
 	static char str[128];
 	int i = 0;
@@ -215,7 +215,7 @@ char *mg_flags_str(uint32_t flags)
 	return str;
 }
 
-char *node_mount_str(uint32_t flags)
+static char *node_mount_str(uint32_t flags)
 {
 	static char str[128];
 	int i = 0;
@@ -247,7 +247,7 @@ char *node_mount_str(uint32_t flags)
 	return str;
 }
 
-int member_int(struct gfsc_node *n)
+static int member_int(struct gfsc_node *n)
 {
 	if (n->flags & GFSC_NF_DISALLOWED)
 		return -1;
@@ -256,7 +256,7 @@ int member_int(struct gfsc_node *n)
 	return 0;
 }
 
-const char *condition_str(int cond)
+static const char *condition_str(int cond)
 {
 	switch (cond) {
 	case 0:
