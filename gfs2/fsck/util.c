8b6731a22d452d76585bac655f7878b51ba38054
Andrew Price <andy@andrewprice.me.uk>
2009-06-10 17:09:25 +0100

    fsck.gfs2: Remove compute_height
    Commit 9bc8ca removed the last two calls to this function so it can be
    removed.
    
    Signed-off-by: Andrew Price <andy@andrewprice.me.uk>

--- gfs2/fsck/util.c
+++ gfs2/fsck/util.c
@@ -11,37 +11,6 @@
 #include "fs_bits.h"
 #include "util.h"
 
-/**
- * compute_height
- * @sdp:
- * @sz:
- *
- */
-int compute_height(struct gfs2_sbd *sdp, uint64_t sz)
-{
-	unsigned int height;
-	uint64_t space, old_space;
-	unsigned int bsize = sdp->sd_sb.sb_bsize;
-	
-	if (sz <= (bsize - sizeof(struct gfs2_dinode)))
-		return 0;
-	
-	height = 1;
-	space = sdp->sd_diptrs * bsize;
-	
-	while (sz > space) {
-		old_space = space;
-		
-		height++;
-		space *= sdp->sd_inptrs;
-		
-		if (space / sdp->sd_inptrs != old_space ||
-			space % sdp->sd_inptrs != 0)
-			break;
-	}
-	return height;
-}
-
 /* Put out a warm, fuzzy message every second so the user     */
 /* doesn't think we hung.  (This may take a long time).       */
 void warm_fuzzy_stuff(uint64_t block)
--- gfs2/fsck/util.h
+++ gfs2/fsck/util.h
@@ -6,7 +6,6 @@
 #define fsck_lseek(fd, off) \
   ((lseek((fd), (off), SEEK_SET) == (off)) ? 0 : -1)
 
-int compute_height(struct gfs2_sbd *sdp, uint64_t sz);
 struct di_info *search_list(osi_list_t *list, uint64_t addr);
 void warm_fuzzy_stuff(uint64_t block);
 const char *block_type_string(struct gfs2_block_query *q);
