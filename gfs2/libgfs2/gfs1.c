cd71fe5817f52f598fa2b5254a39417199e57014
Andrew Price <anprice@redhat.com>
2014-01-27 12:27:50 +0000

    gfs2-utils: Clean up unused functions
    Remove unused functions, some of which call exit() from libgfs2.
    
    Signed-off-by: Andrew Price <anprice@redhat.com>

--- gfs2/libgfs2/gfs1.c
+++ gfs2/libgfs2/gfs1.c
@@ -385,17 +385,3 @@ void gfs_rgrp_out(struct gfs_rgrp *rgrp, struct gfs2_buffer_head *rbh)
 	memcpy(str->rg_reserved, rgrp->rg_reserved, 64);
 	bmodified(rbh);
 }
-
-void gfs_put_leaf_nr(struct gfs2_inode *dip, uint32_t inx, uint64_t leaf_out)
-{
-	uint64_t leaf_no;
-	int count;
-
-	leaf_no = cpu_to_be64(leaf_out);
-	count = gfs1_writei(dip, (char *)&leaf_no, inx * sizeof(uint64_t),
-			   sizeof(uint64_t));
-	if (count != sizeof(uint64_t)) {
-		fprintf(stderr, "gfs_put_leaf_nr:  Bad internal write.\n");
-		exit(1);
-	}
-}
