b9a49e3f91d2beac03c011a536fdb0c7404528bf
Bob Peterson <rpeterso@redhat.com>
2015-09-14 09:55:51 -0500

    libgfs2: Check rgd->bits before referencing it
    This patch adds a check to function gfs2_rgrp_free to make sure
    rgd->bits is non-zero before attempting to reference it.
    This might be NULL because no buffers actually existed because
    it was concocted in an attempt to repair damaged rgrps in fsck.

--- gfs2/libgfs2/rgrp.c
+++ gfs2/libgfs2/rgrp.c
@@ -244,7 +244,7 @@ void gfs2_rgrp_free(struct osi_root *rgrp_tree)
 	while ((n = osi_first(rgrp_tree))) {
 		rgd = (struct rgrp_tree *)n;
 
-		if (rgd->bits[0].bi_bh) { /* if a buffer exists */
+		if (rgd->bits && rgd->bits[0].bi_bh) { /* if a buffer exists */
 			rgs_since_sync++;
 			if (rgs_since_sync >= RG_SYNC_TOLERANCE) {
 				if (!sdp)
