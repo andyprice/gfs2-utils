c78fedfd07a455e934b0a603c9c2e124b9522662
Bob Peterson <rpeterso@redhat.com>
2016-06-24 08:53:41 -0500

    fsck.gfs2: check formal inode number when links go from 1 to 2
    Before commit f2ffb1e, function basic_dentry_checks had access to a
    complete inode tree, so it was able to check the formal inode number
    of every dentry against what it previously found in the inode in
    pass1. But commit f2ffb1e changed function basic_dentry_checks so
    that it bypasses the check if the reference count is 1, which is
    most likely, and it's going to be correct 99% of the time. The
    comments state:
    
    "Since we don't have ii or di, the only way to validate formal_ino
    is to read in the inode, which would kill performance. So skip it
    for now."
    
    The problem is, problems can slip through, undetected, and will
    only be fixed with a second run of fsck.gfs2. For example, during
    testing, I found a set of gfs2 metadata that had two dentries
    pointing to the same dinode. The first dentry encountered by pass2
    was wrong, but it was not checked for this reason. The second
    dentry was correct. The first run of fsck did not catch the problem
    and reacted by setting link count to 2 for the dinode, keeping
    both dentries. The second run found the problem and fixed it,
    changing the link count back to 1 and deleting the bad dentry.
    
    Note that this problem only applies to non-directories, since
    directories will have a value in the dirtree to check against.
    
    This patch solves the problem with a new "innocent until proven
    guilty" approach. When the first dentry reference is found, it is
    assumed to be correct (most files will have a single link).
    When the second dentry reference is found, it goes back and checks
    the original reference. To do that, it takes the (time expensive)
    step of traversing the directory tree, searching every directory
    for the original reference. Once the original dentry is found, it
    checks its formal inode number against the one that has been proven
    correct. This situation ought to be quite rare because the vast
    number of files will have a single correct link. So hopefully only
    valid hard links will cause the slow tree traversal. Once the first
    dentry is found, the tree traversal stops and pass2 continues its
    work.
    
    Signed-off-by: Bob Peterson <rpeterso@redhat.com>
    
    This is a RHEL7 port of this upstream patch:
    https://git.fedorahosted.org/cgit/gfs2-utils.git/commit/?id=ab92c41323babc6b6578cddfb4324ae545927f88
    
    rhbz#1350600

--- gfs2/fsck/link.h
+++ gfs2/fsck/link.h
@@ -4,6 +4,13 @@
 extern struct gfs2_bmap nlink1map; /* map of dinodes with nlink == 1 */
 extern struct gfs2_bmap clink1map; /* map of dinodes w/counted links == 1 */
 
+enum {
+	incr_link_bad = -1,
+	incr_link_good = 0,
+	incr_link_ino_mismatch = 1,
+	incr_link_check_orig = 2,
+};
+
 int link1_set(struct gfs2_bmap *bmap, uint64_t bblock, int mark);
 int set_di_nlink(struct gfs2_inode *ip);
 int incr_link_count(struct gfs2_inum no, struct gfs2_inode *ip,
