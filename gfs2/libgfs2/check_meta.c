3f1b63ed932636ed1232a652fe2e40574b798ad7
Andrew Price <anprice@redhat.com>
2021-03-22 16:01:26 +0000

    libgfs2: Implement meta.c checks as unit tests
    Move the "self-tests" from meta.c into check_meta.c.
    
    Signed-off-by: Andrew Price <anprice@redhat.com>

--- gfs2/libgfs2/check_meta.c
+++ gfs2/libgfs2/check_meta.c
@@ -3,9 +3,56 @@
 
 Suite *suite_meta(void);
 
-START_TEST(test_lgfs2_meta)
+START_TEST(check_metadata_sizes)
 {
-	ck_assert(lgfs2_selfcheck() == 0);
+	unsigned offset;
+	int i, j;
+
+	for (i = 0; i < lgfs2_metadata_size; i++) {
+		const struct lgfs2_metadata *m = &lgfs2_metadata[i];
+		offset = 0;
+		for (j = 0; j < m->nfields; j++) {
+			const struct lgfs2_metafield *f = &m->fields[j];
+			ck_assert(f->offset == offset);
+			offset += f->length;
+		}
+		ck_assert(offset == m->size);
+	}
+}
+END_TEST
+
+START_TEST(check_symtab)
+{
+	int i, j;
+
+	for (i = 0; i < lgfs2_metadata_size; i++) {
+		const struct lgfs2_metadata *m = &lgfs2_metadata[i];
+		for (j = 0; j < m->nfields; j++) {
+			const struct lgfs2_metafield *f = &m->fields[j];
+			if (f->flags & (LGFS2_MFF_MASK|LGFS2_MFF_ENUM))
+				ck_assert(f->symtab != NULL);
+			if (f->symtab)
+				ck_assert(f->flags & (LGFS2_MFF_MASK|LGFS2_MFF_ENUM));
+		}
+	}
+}
+END_TEST
+
+START_TEST(check_ptrs)
+{
+	int i, j;
+
+	for (i = 0; i < lgfs2_metadata_size; i++) {
+		const struct lgfs2_metadata *m = &lgfs2_metadata[i];
+		for (j = 0; j < m->nfields; j++) {
+			const struct lgfs2_metafield *f = &m->fields[j];
+			if (f->flags & LGFS2_MFF_POINTER) {
+				ck_assert(f->points_to != 0);
+			} else {
+				ck_assert(f->points_to == 0);
+			}
+		}
+	}
 }
 END_TEST
 
@@ -13,8 +60,10 @@ Suite *suite_meta(void)
 {
 	Suite *s = suite_create("meta.c");
 
-	TCase *tc_meta = tcase_create("Metadata description self-check");
-	tcase_add_test(tc_meta, test_lgfs2_meta);
+	TCase *tc_meta = tcase_create("Metadata description checks");
+	tcase_add_test(tc_meta, check_metadata_sizes);
+	tcase_add_test(tc_meta, check_symtab);
+	tcase_add_test(tc_meta, check_ptrs);
 	suite_add_tcase(s, tc_meta);
 
 	return s;
